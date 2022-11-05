cdfdd488b17edea72a1a239391d9c33b91621c67
Sardor7314803 <sardor4803uz@gmail.com>
2022-11-05 23:45:14 +0500

    done

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { withAuthenticator } from'@aws-amplify/ui-react'
+
+
+function App({ signOut, user }) {
+  return (
+    <div className="App">
+      {user.attributes.email}
+      <button onClick={signOut}>Sign out</button>
+    </div>
+  );
+}
+
+export default  withAuthenticator(App, {
+  socialProviders: ['apple']
+})
---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals'
+import '@aws-amplify/ui-react/styles.css'
+
+import Amplify from 'aws-amplify'
+import config from './aws-exports';
+import { AmplifyProvider } from '@aws-amplify/ui-react'
+
+Amplify.configure(config)
+const theme = {
+  name: 'pretty-princess',
+  tokens: {
+    colors: {
+      background: {
+        primary: { value: '#eee '}
+      }
+    }
+  }
+}
+ReactDOM.render(
+  <AmplifyProvider theme={theme}>
+    <App />
+  </AmplifyProvider>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
